a44567fdd25c96e6912134ca3858c1d808b34f0a
ggpwnkthx <ibjessup@gmail.com>
2017-11-14 13:52:11 -0500

    Fixed an Error

--- __init__.py
+++ __init__.py
@@ -1,3 +1,13 @@
 # pyflakes: disable-all
 from .main import *
 from .views import *
+
+import aj
+
+def init(plugin_manager):
+    aj.config.load()
+    if 'tinydb' not in aj.config.data:
+        aj.config.data['tinydb'] = "/ajenti-persistent"
+    if aj.config.data['tinydb'] is "":
+        aj.config.data['tinydb'] = "/ajenti-persistent"
+    aj.config.save()
