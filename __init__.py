e724d5c80bb4887cb3517218fe77b9ed55f91719
ggpwnkthx <ibjessup@gmail.com>
2017-11-16 13:31:07 -0500

    tinydb

--- __init__.py
+++ __init__.py
@@ -6,3 +6,8 @@ import aj
 
 def init(plugin_manager):
     aj.config.load()
+    if 'tinydb' not in aj.config.data:
+        aj.config.data['tinydb'] = "/ajenti-persistent"
+    if aj.config.data['tinydb'] is "":
+        aj.config.data['tinydb'] = "/ajenti-persistent"
+    aj.config.save()
